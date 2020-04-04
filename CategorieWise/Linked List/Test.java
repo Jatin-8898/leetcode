863e225f7d0a889945ecf902375f5ca089940ed8
Jatin Varlyani <varlyanijatin88@gmail.com>
2020-04-04 14:34:07 +0530

    Removed unwanted folder

--- CategorieWise/Linked List/Test.java
+++
@@ -1,9 +0,0 @@
-class Test{
-    public static void main(String[] args) {
-        char c = 'a';
-        int ans = 0;
-        ans = ans * 10 + (c - '0');
-        System.out.println(ans);
-
-    }
-}
