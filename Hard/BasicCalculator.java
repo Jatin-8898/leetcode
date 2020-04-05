8b39092bee4bbd7da33ddbfba45c2b050c2093c2
Jatin Varlyani <varlyanijatin88@gmail.com>
2020-04-05 16:01:33 +0530

    Added some more Hard questions :tada:

--- Hard/BasicCalculator.java
+++ Hard/BasicCalculator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Calculator {
+class BasicCalculator {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
